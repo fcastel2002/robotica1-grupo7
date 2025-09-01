73f7c29c9042293ec84eb21ef28fb2566be287f0
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2025-09-01 17:19:13 -0300

    Ejercicio 1 y 2.

---
+++ TP-04/IRB910SC_ej2.m
@@ -0,0 +1,30 @@
+DH = [ 0    0.45    0.075      -pi/2   0;
+       0    0       0.3   0      0;
+       0    0       0.075  -pi/2   0;
+       0    0.32    0       pi/2   0;
+       0    0       0      -pi/2   0;
+       0    0.008   0       0      0 ];
+R = SerialLink(DH);
+q = [0,0,0,0,0,0];
+T = R.fkine(q);
+Q = [0, 0, 0, 0, 0, 0;
+     pi/4, -pi/2, 0 ,0 ,0 ,0;
+    pi/5,-2*pi/5,-pi/10,pi/2,3*pi/10,-pi/2;
+    -0.61, -0.15, -0.3, 1.4,1.9,-1.4];
+for k =1:size(Q,1)
+    T=R.fkine(Q(k,:));
+    fprintf('Configuracion %d\n',k);
+    disp(T)
+end
+
+%% == SCARA TP3 == %%
+
+% dh_scara = [    0   0.262   0.200     pi     0;   % eje 1
+%                 0   0       0.250     0      0;   % eje 2
+%                 0   0       0        0      1;   % eje 3 (P)
+%                 0   0       0        0      0];  % eje 4
+% R1 = SerialLink(dh_scara);
+% q = [0,0,0,0];
+% T = R1.fkine(q);
+% disp("Scara EJ5 tp3\n");
+% disp(T)
