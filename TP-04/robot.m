08f35eb1ddae09f567c6f6054493de6694faaa1d
fcastel2002 <146264604+fcastel2002@users.noreply.github.com>
2025-09-05 15:51:31 -0300

    matriz tool, matriz base en robot.m, esp_trab en plano xy corregido

---
+++ TP-04/robot.m
@@ -0,0 +1,36 @@
+%Trabajo practico N° 3: definición del robot
+
+%definimos matriz DH
+
+% dh = [tita d a alfa sigma]
+
+dh = [ 0    0.290   0      -pi/2   0;
+       0    0       0.270   0      0;
+       0    0       0.070  -pi/2   0;
+       0    0.302   0       pi/2   0;
+       0    0       0      -pi/2   0;
+       0    0.072   0       0      0 ];
+
+
+
+R1 = SerialLink(dh,'name','ABB IRB120 SC #1');
+R2 = SerialLink(dh, 'name', 'ABB IRB120 SC #2');
+R1.qlim = deg2rad([ -165  165;
+                   -110  110;
+                   -110  70;
+                   -160  160;
+                   -120  120;
+                   -400  400 ]);
+R2.qlim = R1.qlim;
+R1.base = transl(0.490+0.09,0,0)*trotz(pi); % la medida de separcion de 980mm divido/2 sumado el radio de la base.
+R2.base = transl(-0.490-0.09,0,0);
+R1.tool = transl(0,0,0.15);
+R2.tool = transl(0,0,0.15);
+R1.offset = [0, -pi/2, 0, 0, 0, 0];
+R2.offset = R1.offset;
+robots = {R1, R2};
+limx = 1.3;
+limy = 1.3;
+limz = 1.3;
+workspace = [-limx limx -limy limy 0 limz];
+
